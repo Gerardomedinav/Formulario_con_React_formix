6d1ba71f2b247f6e726645b00ad7448b1a9a8173
Gerardomediv <gerardomedinav@hotmail.es>
2023-11-27 22:04:08 -0300

    crear validaciones con formik

--- src/components/Registro/Registro.js
+++ src/components/Registro/Registro.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Button, FormControl, FormErrorMessage, FormLabel, Input, VStack } from '@chakra-ui/react';
 import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 import './Registro.css';
 
 const Registro = () => {
+  const [formularioEnviado, setFormularioEnviado] = useState(false);
+
+  const validationSchema = Yup.object().shape({
+    nombre: Yup.string().required('El nombre es obligatorio'),
+    apellido: Yup.string().required('El apellido es obligatorio'),
+    email: Yup.string().email('Formato de correo electrónico inválido').required('El email es obligatorio'),
+    telefono: Yup.string().required('El teléfono es obligatorio'),
+    password: Yup.string().min(6, 'La contraseña debe tener al menos 6 caracteres').required('La contraseña es obligatoria'),
+    confirmarPassword: Yup.string().oneOf([Yup.ref('password'), null], 'Las contraseñas deben coincidir').required('Confirmar la contraseña es obligatorio'),
+  });
+
   return (
     <Box p={8} bg="blue.500" w="100%" h="100vh" display="flex" justifyContent="center" alignItems="center">
       <Box bg="teal.200" p={8} borderRadius="md" width={{ base: '90%', sm: '80%', md: '60%' }}>
         <Formik
           initialValues={{ nombre: '', apellido: '', email: '', telefono: '', password: '', confirmarPassword: '' }}
-          onSubmit={(values) => {
+          onSubmit={(values, actions) => {
             console.log(values);
+            setFormularioEnviado(true);
+            setTimeout(() => {
+              actions.resetForm();
+              setFormularioEnviado(false);
+              actions.setSubmitting(false);
+            }, 3000);
           }}
+          validationSchema={validationSchema}
         >
           {(props) => (
             <Form>
@@ -70,7 +89,14 @@ const Registro = () => {
                     </FormControl>
                   )}
                 </Field>
-                <Button colorScheme="blue" _hover={{ bg: 'green.500' }} type="submit" isLoading={props.isSubmitting}>
+                {formularioEnviado && <Box color="green">¡Formulario enviado con éxito!</Box>}
+                <Button
+                  colorScheme="blue"
+                  _hover={{ bg: 'green.500' }}
+                  type="submit"
+                  isLoading={props.isSubmitting}
+                  isDisabled={!props.isValid}
+                >
                   Registrarse
                 </Button>
               </VStack>
